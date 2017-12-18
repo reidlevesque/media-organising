7028b4d68e193ecb0651a04496ea80dc2e502880
Reid Levesque <reid.levesque@gmail.com>
2017-12-18 14:01:18 -0500

    Added mounting sdcard to the wrapper scripts.

--- wrappers/copyPictures.bat
+++ wrappers/copyPictures.bat
@@ -1,3 +1,3 @@
 @echo off
 
-bash --login -i -c "$HOME/media-organising/src/copyPictures.pl"
+bash --login -i -c "sudo mount -t drvfs F: /mnt/f; $HOME/media-organising/src/copyPictures.pl; sudo umount /mnt/f"
--- wrappers/copyVideos.bat
+++ wrappers/copyVideos.bat
@@ -1,3 +1,3 @@
 @echo off
 
-bash --login -i -c "$HOME/media-organising/src/copyVideos.pl"
+bash --login -i -c "sudo mount -t drvfs F: /mnt/f; $HOME/media-organising/src/copyVideos.pl; sudo umount /mnt/f"
