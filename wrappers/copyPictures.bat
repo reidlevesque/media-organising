ae90aabff5e4ccca743e9197983b234b0036d4c9
Reid Levesque <reid.levesque@gmail.com>
2017-12-18 21:19:00 -0500

    Added a prompt on the wrappers.

--- wrappers/copyPictures.bat
+++ wrappers/copyPictures.bat
@@ -1,3 +1,11 @@
 @echo off
 
+set c=N
+set /P c=Do you need to mount the SD Card (Y/[N])?
+if /I "%c%" EQU "Y" goto :sdcard
+::else
+bash --login -i -c "$HOME/media-organising/src/copyPictures.pl"
+exit
+
+:sdcard
 bash --login -i -c "sudo mount -t drvfs F: /mnt/f; $HOME/media-organising/src/copyPictures.pl; sudo umount /mnt/f"
